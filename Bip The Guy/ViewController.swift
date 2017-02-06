797cbbdf7e8ad321cbc31595886e9186e7ae17ed
Liuke Yang <liukeyang@MacBook-Pro.local>
2017-02-06 16:33:38 -0500

    Bip The Guy (submit)

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -66,11 +66,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func showAlert(title: String, message: String){
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
         let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
         alertController.addAction(defaultAction)
-       
         present(alertController, animated: true, completion: nil)
     }
     
